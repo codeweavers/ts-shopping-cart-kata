62060e5dae01dacb443426b3a1d2d0e3f5a9d65c
Ben Gooding <108468556+benjamin-gooding@users.noreply.github.com>
2023-08-23 13:13:22 +0100

    Improving initial test & fixing incorrect wording. (#1)
    * improving initial tests.
    
    * adding missing type definition.

--- src/receipt.ts
+++ src/receipt.ts
@@ -1,5 +1,11 @@
-import { Product } from './product';
+import {Product} from './product';
+
+export interface ReceiptItem {
+    product: Product;
+    quantity: number;
+}
+
 export class Receipt {
-    public readonly items: Array<{ product: Product, quantity: number }> = [];
+    public readonly items: Array<ReceiptItem> = [];
     public readonly totalPrice: number = 0;
 }
